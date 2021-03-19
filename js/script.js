3fe039823a8bdc41d9413946cd1ff6ca2e030b76
xKOOOTx <kooot123@gmail.com>
2021-03-19 14:56:45 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 const url = 'https://my-json-server.typicode.com/Vespand/crmm-tasks/users';
 
 const arr = [];
-const newArray = [];
+const toHighArr = [];
+const toLowArr = [];
+
+
 
 let status = function (response) {
     if (response.status !== 200) {
@@ -16,88 +19,61 @@ let json = function (response) {
 fetch(url)
     .then(status)
     .then(json)
-    .then(function (data) {
+/*    .then(function (data) {
             for(let i = 0; i < data.length; i++) {
                 arr.push(data[i])
+                render()
             }
         }
-    )
+    )*/
+    .then(function(data) {
+        arr.push(data)
+        render(data)
+        setTimeout(() => {
+            let low  = data.sort((a, b) => a.rating > b.rating ? 1 : -1);
+            low.push(toLowArr)
+        },1)
+        console.log(data)
+    })
     .catch(function (error) {
         console.log('error', error)
     })
 
-setTimeout(() => {
-    render();
-}, 500)
-console.log(arr)
-
-function render() {
-    arr.forEach(function (arrayItem) {
+function render(data) {
+    data.forEach(function (arrayItem) {
         let id = arrayItem.id;
         let name = arrayItem.name;
         let description = arrayItem.description;
         let avatar = arrayItem.avatar;
         let rating = arrayItem.rating;
+        let idx = arr.indexOf(arrayItem) + 1;
         let mainContent = document.getElementById('mainContent')
-
-
-        mainContent.insertAdjacentHTML('afterbegin', (
+        let div =
             `
-                <div class="content__wrapper flex" id="contentWrapper">
-                    <div class="flex">
-                        <div class="content__avatar" style="background: url(${avatar}) center no-repeat; background-size: cover;"></div>
-                            <div class="content__initials">
-                                <h3>Пользователь:</h3>
-                                <p>${name}</p>
+                    <div class="content__wrapper flex" id="contentWrapper">
+                        <div class="flex">
+                            <div class="content__avatar" style="background: url(${avatar}) center no-repeat; background-size: cover;"></div>
+                                <div class="content__initials">
+                                    <h3>Пользователь:</h3>
+                                    <p>${name}</p>
+                            </div>
+                        </div>
+                        <div class="content__rating">
+                            <img src="img/star.png" alt="rating_star">
+                            <p class="content__rating_value">${rating}</p>
                         </div>
                     </div>
-                    <div class="content__rating">
-                        <img src="img/star.png" alt="rating_star">
-                        <p class="content__rating_value">${rating}</p>
-                    </div>
-                </div>
-            `
-        ))
-
-        /*mainContent.innerHTML =
-            `
-                <div class="content__wrapper flex">
-                    <div class="flex">
-                        <div class="content__avatar" style="background: url(${avatar}) center no-repeat; background-size: cover;"></div>
-                            <div class="content__initials">
-                                <h3>Пользователь:</h3>
-                                <p>${name}</p>
-                        </div>
-                    </div>
-                    <div class="content__rating">
-                        <img src="img/star.png" alt="rating_star">
-                        <p class="content__rating_value">${rating}</p>
-                    </div>
-                </div>
-            `*/
+                `;
+        mainContent.insertAdjacentHTML('afterbegin', '')
+        mainContent.insertAdjacentHTML('afterbegin', div)
     })
 }
 
 const sortToHighBtn = document.getElementById('sortToHighBtn');
 const sortToLowBtn = document.getElementById('sortToLowBtn');
-const contentWrapper = document.getElementById('contentWrapper')
 
-sortToLowBtn.addEventListener('click', () => {
-    function sortByRatingHigherToLower(arr) {
-        arr.sort((a, b) => a.rating > b.rating ? 1 : -1);
-    }
-    sortByRatingHigherToLower(arr);
-    render();
-})
-sortToHighBtn.addEventListener('click', () => {
-    function sortByRatingLowerToHigher(arr) {
-        arr.sort((a, b) => a.rating < b.rating ? 1 : -1);
-    }
-    sortByRatingLowerToHigher(arr);
-    render();
-})
-
-
-
-
+// function sortToLow(data) {
+//     data.sort((a, b) => a.rating > b.rating ? 1 : -1);
+//     render(data);
+// }
 
